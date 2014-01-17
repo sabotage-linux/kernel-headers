9aa9f123f5d285ac1b04e3f3bf440e96b2922bcf
Justin Cormack <justin@specialbusservice.com>
2014-01-17 22:10:14 +0000

    symlink shared headers

--- UPDATE.sh
+++ UPDATE.sh
@@ -5,12 +5,23 @@
 # first argument is kernel tree, you need to
 # make allnoconfig
 # make headers_install_all
-# first to get headers in $1/usr/include
+# there first to get headers in $1/usr/include
 
 if [ "$#" -ne 1 ] || ! [ -d "$1" ]; then
   echo "Usage: $0 KERNELTREE" >&2
   exit 1
 fi
+
+git rm -r generic/include/*
+mkdir -p generic/include
+for dir in arch asm-generic drm linux mtd rdma scsi sound uapi video xen
+do
+  cp -a $1/usr/include/$dir generic/include/
+done
+
+find generic -name '..install.cmd' | xargs rm
+find generic -name '.install' | xargs rm
+git add generic/include/
 
 for arch in arm powerpc mips x86
 do
@@ -18,12 +29,11 @@ do
   mkdir -p $arch/include
   for dir in arch asm-generic drm linux mtd rdma scsi sound uapi video xen
   do
-    cp -a $1/usr/include/$dir $arch/include/
+    ln -s ../../generic/include/$dir $arch/include/$dir
   done
   cp -a $1/usr/include/asm-$arch $arch/include/asm
-  # de-cruft
-  find . -name '..install.cmd' | xargs git rm
-  find . -name '.install' | xargs git rm
+  find $arch -name '..install.cmd' | xargs rm
+  find $arch -name '.install' | xargs rm
   git add $arch/include
 done
 
