26014dca5f2210f75df4fa8c1a375a37792fae55
rofl0r <retnyg@gmx.net>
2014-01-20 21:12:24 +0100

    add script to create release tarball

---
+++ create-dist.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+if [ -z "$VER" ] ; then
+	echo set VER!
+	exit
+fi
+me=`pwd`
+
+proj=kernel-headers
+projver=${proj}-${VER}
+repo="$me"
+
+tempdir=/tmp/${proj}-0000
+rm -rf $tempdir
+mkdir -p $tempdir
+
+cd $tempdir || { echo ERROR CDing ; exit 1 ; }
+git clone $repo $projver
+#(cd $projver ; git checkout v$VER )
+rm -rf $projver/.git
+rm -rf $projver/.gitignore
+rm -rf $projver/docs
+rm $projver/*.sh
+
+tar cJf $proj.tar.xz $projver/
+mv $proj.tar.xz $me/$projver.tar.xz
+echo done
