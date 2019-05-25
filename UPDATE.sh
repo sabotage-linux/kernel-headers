924b0e8af9ecd01f6d0f92440870a7fb3b3982f8
rofl0r <rofl0r@users.noreply.github.com>
2019-05-25 18:01:20 +0100

    UPDATE.sh: fix compat with newer kernel tree layout

--- UPDATE.sh
+++ UPDATE.sh
@@ -12,15 +12,21 @@ if [ "$#" -ne 1 ] || ! [ -d "$1" ]; then
   exit 1
 fi
 
-git rm -r generic/include/*
+git rm -rf generic/include/*
 mkdir -p generic/include
-for dir in arch asm-generic drm linux mtd rdma scsi sound uapi video xen
+for dir in asm-generic drm linux mtd rdma scsi sound video xen uapi
 do
-  cp -a $1/usr/include/$dir generic/include/
+  if test -d $1/usr/include/$dir ; then
+    cp -a $1/usr/include/$dir generic/include/
+  else
+    echo "warning: skipping $dir"
+  fi
 done
 
+uapi_base=
+test -d $1/usr/include/uapi && uapi_base=uapi/
 # these headers are missing from headers_install_all
-cp -a $1/include/uapi/linux/{a.out,kvm,kvm_para,module}.h generic/include/linux/
+cp -a $1/include/${uapi_base}linux/{a.out,kvm,kvm_para,module}.h generic/include/linux/
 
 find generic -name '..install.cmd' -delete
 find generic -name '.install' -delete
@@ -28,13 +34,18 @@ git add generic/include/
 
 for arch in arm arm64 powerpc mips x86 microblaze openrisc
 do
-  git rm -r $arch/include/*
+  git rm -rf $arch/include/*
   mkdir -p $arch/include
   for dir in arch asm-generic drm linux mtd rdma scsi sound uapi video xen
   do
-    ln -s ../../generic/include/$dir $arch/include/$dir
+    test -d generic/include/$dir && \
+      ln -s ../../generic/include/$dir $arch/include/$dir
   done
-  cp -a $1/usr/include/asm-$arch $arch/include/asm
+  if test -d $1/usr/include/asm-$arch ; then
+    cp -a $1/usr/include/asm-$arch $arch/include/asm
+  else
+    cp -a $1/usr/include/arch-$arch/asm $arch/include/
+  fi
   find $arch -name '..install.cmd' -delete
   find $arch -name '.install' -delete
   git add $arch/include
